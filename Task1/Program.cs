25cda4fbacd064ffed6c9a74b479570b3f25e44b
Mykola Gomolach <ipzm222_gmm@student.ztu.edu.ua>
2022-11-10 05:33:49 +0200

    Will change the first three lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Line one");
+Console.WriteLine("Line two");
+Console.WriteLine("Line three");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
